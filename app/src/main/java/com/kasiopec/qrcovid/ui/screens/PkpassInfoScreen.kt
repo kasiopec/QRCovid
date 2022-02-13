f66c195b57219a85f8350820c145ad79904ea7bb
kasiopec <kasiclysm@gmail.com>
2022-02-13 10:32:29 +0200

    Added basic labels to info screen

--- app/src/main/java/com/kasiopec/qrcovid/ui/screens/PkpassInfoScreen.kt
+++ app/src/main/java/com/kasiopec/qrcovid/ui/screens/PkpassInfoScreen.kt
@@ -1,11 +1,13 @@
 package com.kasiopec.qrcovid.ui.screens
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import com.kasiopec.qrcovid.app_components.BottomBarNavigator
 import com.kasiopec.qrcovid.ui.theme.QRCovidTheme
@@ -36,4 +38,25 @@ fun PkpassInfoScreen(navController: NavController) {
             }
         }
     }
+}
+
+
+@Composable
+fun InfoBlock(){
+    Column(){
+        Text(text = "Description:")
+        Text(text = "Organization Name:")
+        Text(text = "Vaccination day:")
+        Text(text = "User name:")
+        Text(text = "Birthday date:")
+        Text(text = "Vaccine dose:")
+    }
+
+}
+
+
+@Preview
+@Composable
+fun PreviewComposable(){
+    InfoBlock()
 }
